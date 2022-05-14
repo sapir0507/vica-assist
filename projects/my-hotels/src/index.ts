3345a331f5d4cfe31a586d1c63cb432432e55337
sapir0507 <sapir.s@info.cts.org.il>
2022-05-14 11:32:30 +0300

    a

--- projects/my-hotels/src/index.ts
+++ projects/my-hotels/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/my-hotels';
-export * from './lib/my-hotels/my-hotels.service';
 export * from './lib/my-hotels/my-hotels.component'
 export * from './lib/my-hotels/my-hotels.module';
